9cecc98da370284d54eafcde5e4270e2bed1a4e9
kdelavan <78820584+kdelavan@users.noreply.github.com>
2021-05-24 14:11:56 -0700

    Revert "Revert "added synonym component""
    This reverts commit 064c8229b461bdf1f2c1eee1ddf78ac7e2b9c857.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -8,9 +9,12 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
-              {definition.example}
+              <Synonyms synonyms={definition.synonyms} />
+              <strong>Example: </strong>
+              <em>{definition.example}</em>
             </p>
           </div>
         );

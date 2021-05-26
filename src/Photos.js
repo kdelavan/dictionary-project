94eb9c51de8f309d3c80537784d71fd9dd68a7c2
kdelavan <78820584+kdelavan@users.noreply.github.com>
2021-05-26 15:06:57 -0700

    changed alt img text

--- src/Photos.js
+++ src/Photos.js
@@ -18,7 +18,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt="image related to keyword"
+                    alt="related to keyword"
                   />
                 </a>
               </div>
